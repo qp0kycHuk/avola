09bdae4fbbd1423739710f0ffc9c6b3b566824ed
qp0kycHuk <serega990112@mail.ru>
2022-07-19 14:07:35 +0300

    move html to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,22 @@ const htmlWebpackPluginDefaults = {
   inject: 'head'
 }
 
+const pages = [
+  '404.html', 'account.html', 'auth-step-1.html', 'auth-step-forgotpass.html', 'auth-step-sign-in.html', 'buners.html',
+  'filter.html', 'index.html', 'jk-inner.html', 'jk-inner-disabled.html', 'lk-archive.html', 'lk-designing.html', 
+  'lk-jk-inner.html', 'lk-none.html', 'lk-object(Доступ запрещен).html', 'lk-object.html', 'lk-object-item.html',
+  'lk-request-add.html', 'lk-request-expertiza.html', 'lk-request-podbor.html', 'lk-request-prosmotr.html', 
+  'lk-requests-objects.html', 'lk-saved-collections.html', 'lk-statistic.html', 'lk-statistic-object.html', 
+  'lk-team.html', 'lk-users.html', 'lk-view.html', 'objects.html'
+]
+
+const dialogs = [
+  'confirm-modal.html', 'dialog-calculator.html', 'dialog-form-width-dude.html',
+  'dialog-width-dude.html', 'filter-help-modal.html', 'lk-modal-profile.html', 
+  'lk-user-edit-modal.html', 'lk-users-modal-expertiza.html', 'lk-users-modal-prosmotr.html', 
+  'modal-request-add.html', 'new-success-dialog.html', 'phone-request.html'
+]
+
 module.exports = {
   entry: './src/index.js',
   resolve: {
@@ -55,19 +71,17 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: "css/style.css", }),
-    // new HtmlWebpackPlugin({
-    //   ...htmlWebpackPluginDefaults,
-    //   template: './src/index.html',
-    //   filename: 'index.html'
-    // }),
-    // new HtmlWebpackPlugin({
-    //   ...htmlWebpackPluginDefaults,
-    //   template: './src/ui.html',
-    //   filename: 'ui.html'
-    // }),
+    ...pages.map((name) => new HtmlWebpackPlugin({
+      ...htmlWebpackPluginDefaults,
+      template: `./src/html_pages/${name}`,
+      filename: name
+    })),
     new CopyPlugin({
       patterns: [
         { from: "./src/img/", to: "./img/" },
+        { from: "./src/js/", to: "./js/" },
+        { from: "./src/source/", to: "./source/" },
+        ...dialogs.map((name) => ({ from: `./src/html_dialogs/${name}`, to: `./${name}` }))
         // { from: "./src/dialog-small.html", to: "./dialog-small.html" },
         // { from: "./src/dialog-middle.html", to: "./dialog-middle.html" },
         // { from: "./src/dialog-large.html", to: "./dialog-large.html" },
